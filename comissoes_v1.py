c30fe8ceec0733460ea3287d3a13abce3f76adf3
Roberto Flor <roberto.ht2ml@gmail.com>
2023-08-22 15:54:25 -0300

    versão funcional

--- comissoes_v1.py
+++ comissoes_v1.py
@@ -2,29 +2,35 @@
 import pandas as pd
 import numpy as np
 import csv
-import unicodedata  # Biblioteca para retirar acentos
+from unidecode import unidecode
 import pandas_gbq
 from google.cloud import bigquery
+import locale
 
-# # Autenticação do Google Cloud
-# # gcloud auth login
+# Defina a localização para o formato que utiliza ponto para separar os milhares e vírgula para separar decimais
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-# """# Compensação"""
-# # In[]
-# compensacao = pd.read_csv(r'C:\Users\roberto.flor\Documents\comissões\compensacao.csv', sep=';')
+# Autenticação do Google Cloud
+# gcloud auth login
 
-# # Função para retirar acentos, substituir espaços por underscores, remover pontos e converter para minúsculas
-# def formatar_nome_coluna(nome_coluna):
-#     nome_sem_acentos = unicodedata.normalize('NFD', nome_coluna)  # Remove acentos
-#     nome_sem_pontos = nome_sem_acentos.replace('.', '')  # Remove pontos
-#     nome_formatado = nome_sem_pontos.replace(' ', '_')  # Substitui espaços por underscores
-#     nome_formatado_minusculo = nome_formatado.lower()  # Converte para minúsculas
-#     return nome_formatado_minusculo
+"""# Compensação"""
+# In[]
+compensacao = pd.read_csv(r'C:\Users\roberto.flor\Documents\comissões\CustomerLineItems.csv', sep=';')
 
-# # Aplica a função a todos os nomes de coluna
-# compensacao.columns = [formatar_nome_coluna(nome) for nome in compensacao.columns]
-# # In[]
-# # Especifique o projeto e o dataset do BigQuery
+# Função para retirar acentos, substituir espaços por underscores, remover pontos e converter para minúsculas
+def formatar_nome_coluna(nome_coluna):
+    nome_sem_acentos = unidecode(nome_coluna)  # Remove acentos
+    nome_sem_pontos = nome_sem_acentos.replace('.', '')  # Remove pontos
+    nome_formatado = nome_sem_pontos.replace(' ', '_')  # Substitui espaços por underscores
+    nome_formatado_minusculo = nome_formatado.lower()  # Converte para minúsculas
+    return nome_formatado_minusculo
+
+# Aplica a função a todos os nomes de coluna
+compensacao.columns = [formatar_nome_coluna(nome) for nome in compensacao.columns]
+
+compensacao.to_csv('compensacao_bigquery.csv',encoding='utf-8',sep=';')
+# In[]
+# Especifique o projeto e o dataset do BigQuery
 # projeto = 'dotted-cedar-149302'
 # dataset = 'laboratorio'
 
@@ -37,53 +43,68 @@ from google.cloud import bigquery
 
 """# Demonstrativo"""
 
-demonstrativo_1000 = pd.read_csv(r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\demonstrativo_empresa_1000.csv', sep=';',dtype={'a_vencer_liq_simb_': str, 'Montante (ME)': str})
-demonstrativo_3000 = pd.read_csv(r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\demonstrativo_empresa_3000.csv', sep=';',dtype={'a_vencer_liq_simb_': str, 'Montante (ME)': str})
-demonstrativo_4000 = pd.read_csv(r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\demonstrativo_empresa_4000.csv', sep=';',dtype={'a_vencer_liq_simb_': str, 'Montante (ME)': str})
+# In[]
+
+# Carregar o arquivo CSV em um DataFrame
+caminho_arquivo_1000 = r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\FECHAMENTO INTERNO INDUSTRIAL (AGOSTO).csv'
+caminho_arquivo_1000_fat = r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\FECHAMENTO INTERNO INDUSTRIAL (AGOSTO-FAT).csv'
+caminho_arquivo_3000 = r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\FECHAMENTO INTERNO GIGA (AGOSTO).csv'
+caminho_arquivo_4000 = r'C:\Users\roberto.flor\Documents\comissões\demonstrativos\FECHAMENTO INTERNO PROINOX (AGOSTO).csv'
+
+def replace_comma_with_dot(text):
+    if isinstance(text, str):
+        return text.replace("'", '').replace("'", "")
+    return text
+
+demonstrativo_1000 = pd.read_csv(caminho_arquivo_1000, sep=';', encoding='utf-8-sig')
+demonstrativo_1000_fat = pd.read_csv(caminho_arquivo_1000_fat, sep=';', encoding='utf-8-sig')
+demonstrativo_3000 = pd.read_csv(caminho_arquivo_3000, sep=';', encoding='utf-8-sig')
+demonstrativo_4000 = pd.read_csv(caminho_arquivo_4000, sep=';', encoding='utf-8-sig')
+
+demonstrativo = pd.concat([demonstrativo_1000, demonstrativo_3000, demonstrativo_4000,demonstrativo_1000_fat], ignore_index=True)
+
+# Aplicar a função replace_comma_with_dot à coluna valor_do_titulo
+demonstrativo['Valor do título'] = demonstrativo['Valor do título'].apply(replace_comma_with_dot)
+demonstrativo['Valor base da comissão'] = demonstrativo['Valor base da comissão'].apply(replace_comma_with_dot)
+demonstrativo['Valor da comissão'] = demonstrativo['Valor da comissão'].apply(replace_comma_with_dot)
+demonstrativo['Percentual de comissão'] = demonstrativo['Percentual de comissão'].apply(replace_comma_with_dot)
+
+# Converter colunas de datas para o formato datetime
+date_columns = ['Data do documento', 'Data do vencimento', 'Data de compensação', 'Dt.criação']
+for col in date_columns:
+    demonstrativo[col] = pd.to_datetime(demonstrativo[col], errors='coerce')
 
 
+# In[]
 
+columns_selected = [   
+    'Empresa',
+    'Exercício',
+    'Nº documento',
+    'Número do Documento Original',
+    'Nome do representante',
+    'Item',
+    'Tipo de movimento',
+    'Código representante',
+    'Cliente',
+    'Data do documento',
+    'Data de compensação',
+    'Doc.compensação',
+    'Percentual de comissão',
+    'Dt.criação',
+    'Criado por',
+    'Apuração',
+    'Referência cliente',
+    'Denominação Tabelas'
+]
 
-data_selected = demonstrativo[selected_columns]# Transforme o restante das colunas de acordo com o dicionário column_types
-data_transformed = data_selected.astype(column_types)
-
-# Definir os tipos de coluna desejados
-column_types = {
-    "Empresa": "Int64",
-    "Status comp.": "Int64",
-    "Nº ID fiscal 1": "str",
-    "Cliente": "str",
-    "Nome do cliente": "str",
-    "Tipo lçto.contábil": "str",
-    "Dt.lnçmto.cont.": "datetime64[ns]",
-    "Data-base": "datetime64[ns]",
-    "Atribuição": "str",
-    "Referência": "str",
-    "Item visão lçto.": "Int64",
-    "Lançamento contábil": "Int64",
-    "Data vencimento líq.": "datetime64[ns]",
-    "Débito/crédito": "str",
-    "Montante (ME)": "str",
-    "Banco da empresa": "str",
-    "Chave ref.3": "str",
-    "Texto de item": "str",
-    "Lançto.compensação": "str",
-    "Data de compensação": "datetime64[ns]",
-    "Razão especial": "str",
-    "A vencer líq.(símb.)": "str",
-    "Condições pagamento": "str",
-    "Dias de desconto 1": "Int64",
-    "Forma de pagamento": "str",
-    "Referência à fatura": "str",
-    "Nome da região": "str",
-    "PI criada por": "str",
-    "Chave de lançamento": "str"
-}
+# Transforme o restante das colunas de acordo com o dicionário column_types
+demonstrativo_consolidado = demonstrativo[columns_selected].dropna(subset=['Doc.compensação','Código representante'])
 
 
 # Função para retirar acentos, substituir espaços por underscores, remover pontos e converter para minúsculas
 def formatar_nome_coluna(nome_coluna):
-    nome_sem_acentos =unicodedata.normalize('NFD', nome_coluna)   # Remove acentos
+    nome_sem_acentos =unidecode(nome_coluna)   # Remove acentos
     nome_sem_pontos = nome_sem_acentos.replace('.', '')  # Remove pontos
     nome_sem_barras = nome_sem_pontos.replace('/', '_')  # Remove pontos
     nome_sem_abertura_parenteses = nome_sem_barras.replace('(', '_')  # Remove pontos
@@ -92,46 +113,27 @@ def formatar_nome_coluna(nome_coluna):
     nome_formatado_minusculo = nome_formatado.lower()  # Converte para minúsculas
     return nome_formatado_minusculo
 
-data_transformed.columns = [formatar_nome_coluna(nome) for nome in data_transformed.columns]
+demonstrativo_consolidado.columns = [formatar_nome_coluna(nome) for nome in demonstrativo_consolidado.columns]
 
-projeto = 'dotted-cedar-149302'
-dataset = 'laboratorio'
 
-# Nome da tabela no BigQuery
-nome_tabela = 'laboratorio.teste'
+# Função para converter string em float
+def string_to_float(valor_string):
+    return float(valor_string.replace(',', '.'))
 
-# Gravar o DataFrame no BigQuery
-# Defina o esquema das colunas com tipos de dados corretos
-schema = [
-    {'name': 'empresa', 'type': 'INTEGER'},
-    {'name': 'status_comp', 'type': 'INTEGER'},
-    {'name': 'no_id_fiscal_1', 'type': 'STRING'},
-    {'name': 'cliente', 'type': 'STRING'},
-    {'name': 'nome_do_cliente', 'type': 'STRING'},
-    {'name': 'tipo_lctocontabil', 'type': 'STRING'},
-    {'name': 'dtlncmtocont', 'type': 'DATE'},
-    {'name': 'data-base', 'type': 'DATE'},
-    {'name': 'atribuicao', 'type': 'STRING'},
-    {'name': 'referencia', 'type': 'STRING'},
-    {'name': 'item_visao_lcto', 'type': 'INTEGER'},
-    {'name': 'lancamento_contabil', 'type': 'INTEGER'},
-    {'name': 'data_vencimento_liq', 'type': 'DATE'},
-    {'name': 'debito_credito', 'type': 'STRING'},
-    {'name': 'montante__me_', 'type': 'STRING'},
-    {'name': 'banco_da_empresa', 'type': 'STRING'},
-    {'name': 'chave_ref3', 'type': 'STRING'},
-    {'name': 'texto_de_item', 'type': 'STRING'},
-    {'name': 'lanctocompensacao', 'type': 'STRING'},
-    {'name': 'data_de_compensacao', 'type': 'DATE'},
-    {'name': 'razao_especial', 'type': 'STRING'},
-    {'name': 'a_vencer_liq_simb_', 'type': 'STRING'},
-    {'name': 'condicoes_pagamento', 'type': 'STRING'},
-    {'name': 'dias_de_desconto_1', 'type': 'INTEGER'},
-    {'name': 'forma_de_pagamento', 'type': 'STRING'},
-    {'name': 'referencia_a_fatura', 'type': 'STRING'},
-    {'name': 'nome_da_regiao', 'type': 'STRING'},
-    {'name': 'pi_criada_por', 'type': 'STRING'},
-    {'name': 'chave_de_lancamento', 'type': 'STRING'}
-]
 
-pandas_gbq.to_gbq(data_transformed, nome_tabela, project_id=projeto, if_exists='replace', table_schema=schema)
+demonstrativo_consolidado['data_de_compensacao'] = demonstrativo_consolidado['data_de_compensacao'].fillna("Null")
+
+# demonstrativo_consolidado['percentual_de_comissao'] = demonstrativo_consolidado['percentual_de_comissao'].apply(string_to_float)
+demonstrativo_consolidado.to_csv('demonstrativo.csv',encoding='utf-8',sep=';')
+
+# projeto = 'dotted-cedar-149302'
+# dataset = 'laboratorio'
+
+# # Nome da tabela no BigQuery
+# nome_tabela = 'laboratorio.teste'
+
+# # pandas_gbq.to_gbq(demonstrativo_consolidado, nome_tabela, project_id=projeto, if_exists='replace', table_schema=schema)
+# pandas_gbq.to_gbq(demonstrativo_consolidado, nome_tabela, project_id=projeto, if_exists='replace')
+# # %%
+
+# %%
