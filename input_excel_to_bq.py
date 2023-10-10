2a1a47d080d3cdc42b698da9a5076b7d98ca38bf
Roberto Flor <roberto.ht2ml@gmail.com>
2023-10-09 22:06:18 -0300

    update_0910

--- input_excel_to_bq.py
+++ input_excel_to_bq.py
@@ -1,6 +1,7 @@
 # In[]
 
 import pandas as pd
+from datetime import datetime
 import numpy as np
 import csv
 from unidecode import unidecode
@@ -8,7 +9,7 @@ import pandas_gbq
 from google.cloud import bigquery
 import locale
 
-caminho_unificado = r'files/dados dos representantes.xlsx'
+caminho_unificado = r'files/salarios_0910.xlsx'
 
 arquivo = pd.read_excel(caminho_unificado, sheet_name='dados')
 
@@ -31,18 +32,27 @@ arquivo.columns = [formatar_nome_coluna(nome) for nome in arquivo.columns]
 
 # Defina uma lista de tipos de dados correspondentes às colunas
 tipos_de_dados = {
-    'cod_protheus': str,
-    'codigo_sap': str,
-    'codigo_representante': str,
-    'diretor': str,
-    'gerente': str,
-    'email_reps': str,
+'numero_no_cadastro_de_pessoa_f':np.int64,
+'no_pess':np.int64,
+'no_pessoal':str,
+'status_da_ocupacao':str,
+'entrada':'datetime64[ns]',
+'saida':'datetime64[ns]',
+'centro_cst':str,
+'centro_de_custo':str,
+'posicao':str,
+'posicao1':str,
+'montante':np.float64,
+'inicio':'datetime64[ns]',
+'fim':'datetime64[ns]'
 }
 # Use um loop para aplicar os tipos de dados apropriados às colunas
 
 for col, dtype in tipos_de_dados.items():
     arquivo[col] = arquivo[col].astype(dtype)
 esquema = []
+
+
 
 for col_name, col_type in arquivo.dtypes.items():
     if col_type == 'int64':
@@ -57,8 +67,10 @@ for col_name, col_type in arquivo.dtypes.items():
         esquema.append({'name': col_name, 'type': field_type})
 
 projeto = 'rh-analytics-397518'
-dataset = 'referencias'
-tabela = 'referencias.dados_representantes'
+dataset = 'people_analytics'
+tabela = 'people_analytics.salarios'
+
+arquivo['update'] = datetime.now()
 
 pandas_gbq.to_gbq(arquivo, tabela, project_id=projeto, if_exists='replace')
 
